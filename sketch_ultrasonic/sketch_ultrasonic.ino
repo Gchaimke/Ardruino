c91171902e204f2138a8f9b5acd7edca6be7c5e3
Chaim Gorbov <Gchaimke@gmail.com>
2017-07-08 00:39:38 +0300

    using lib ultrasonic

--- sketch_ultrasonic/sketch_ultrasonic.ino
+++ sketch_ultrasonic/sketch_ultrasonic.ino
@@ -1,11 +1,9 @@
 // ОБЪЯВЛЕНИЕ ПЕРЕМЕННЫХ
 // переменные ультразвуковых дальномеров
-int echoPin1 = 2;
-int trigPin1 = 3;
-int echoPin2 = 4;
-int trigPin2 = 5;
-long duration1, duration2, cm1, cm2;
-
+#include <Ultrasonic.h>
+Ultrasonic ultraleft(3,2);   // (Trig PIN,Echo PIN)
+Ultrasonic ultraright(5,4);  // (Trig PIN,Echo PIN)
+long cm1, cm2;
 // переменные двигателей
 int speed1_1 = 6;
 int speed1_2 = 9;
@@ -15,25 +13,18 @@ int moving = 1;
 int rotate = 0;
 int rotate_side = 1;
 //ДВИЖЕНИЕ (min-0 max-255)
-int iSpeed = 70;
-
+int mySpeed = 70;
 // переменные таймеров анти-залипания
 int timer1 = 0;
 int timer2 = 0;
 int timer3 = 0;
 int timer4 = 0;
-
 // переменные вентилятора
 int relay = 12;
 unsigned long endtime = 600000; //автоматическое выключение через 600 секунд
-
 // SETUP программы (выполняется 1 раз, при старте)
 void setup() {
   //назначение портов
-  pinMode(trigPin1, OUTPUT);
-  pinMode(echoPin1, INPUT);
-  pinMode(trigPin2, OUTPUT);
-  pinMode(echoPin2, INPUT);
   pinMode(speed1_1, OUTPUT);
   pinMode(speed1_2, OUTPUT);
   pinMode(speed2_1, OUTPUT);
@@ -54,49 +45,18 @@ void loop() {
 
     //ПЕРВЫЙ ДАЛЬНОМЕР
     //посылается и принимается звуковой сигнал
-    digitalWrite(trigPin1, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigPin1, HIGH);
-    delayMicroseconds(20);
-    digitalWrite(trigPin1, LOW);
-    duration1 = pulseIn(echoPin1, HIGH, 100000);
-    //эти 4 строчки "перезапускают" дальномер (если ваш дальномер так-же,
-    //как и мой, зависает при дальности препятствия свыше 3-х метров.)
-    pinMode(echoPin1, OUTPUT);
-    digitalWrite(echoPin1, LOW);
-    delay(10);
-    pinMode(echoPin1, INPUT);
-    //если дальномер "залип" и показывает "0", то время сигнала выставляется 3 метра.
-    if (duration1 == 0) {
-      duration1 = 17400;
-    }
-    //перевод времени достижения сигнала в сантиметры
-    cm1 = duration1 / 58;
+    cm1 = ultraleft.Ranging(CM);
     //небольшая задержка, для предотвращения смешения сигналов со вторым дальномером
     delay(300);
 
     //ВТОРОЙ ДАЛЬНОМЕР
-    digitalWrite(trigPin2, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigPin2, HIGH);
-    delayMicroseconds(20);
-    digitalWrite(trigPin2, LOW);
-    duration2 = pulseIn(echoPin2, HIGH, 100000);
-    pinMode(echoPin2, OUTPUT);
-    digitalWrite(echoPin2, LOW);
-    delay(10);
-    pinMode(echoPin2, INPUT);
-    if (duration2 == 0) {
-      duration2 = 17400;
-    }
-    cm2 = duration2 / 58;
+    cm2 = ultraright.Ranging(CM);
     delay(300);
 
-    //ОПРЕДЕЛЕНИЕ НАПРАВЛЕНИЯ ДВИЖЕНИЯ
-
+    //ОПРЕДЕЛЕНИЕ НАПРАВЛЕНИЯ ДВИЖЕНИЯ ( управление моторами )
     //нет препятствия
     if (cm1 >= 10 && cm2 >= 10) {
-      wheelsMove(1, 1, iSpeed);
+      wheelsMove(1, 1, mySpeed);
       delay(100);
       timer1 = timer1 + 1;
       timer2 = 0;
@@ -106,7 +66,7 @@ void loop() {
 
     //далеко нет препятствий
     if (cm1 >= 40 && cm2 >= 40) {
-      wheelsMove(1, 1, iSpeed);
+      wheelsMove(1, 1, mySpeed);
       delay(100);
       timer4 = timer4 + 1;
       timer1 = 0;
@@ -116,14 +76,14 @@ void loop() {
 
     //препятствие (объезд вправо)
     if (cm1 > 40 && cm2 < 30 && moving != 5) {
-      wheelsMove(1, 0.7, iSpeed);
+      wheelsMove(1, 0.7, mySpeed);
       rotate = random(400, 800);
       delay(rotate);
     }
 
     //препятствие (объезд влево)
     if (cm1 < 30 && cm2 > 40 && moving != 6) {
-      wheelsMove(0.7, 1, iSpeed);
+      wheelsMove(0.7, 1, mySpeed);
       rotate = random(400, 800);
       delay(rotate);
     }
@@ -142,12 +102,12 @@ void loop() {
     if (moving == 2 || timer1 > 25 || timer3 > 2 || timer4 > 50) {
       rotate_side = random(1, 3); //рандомная сторона поворота
       if (rotate_side == 1) {
-        wheelsMove(0, 1, iSpeed);
+        wheelsMove(0, 1, mySpeed);
         rotate = random(400, 800);
         delay(rotate);
       }
       if (rotate_side == 2) {
-        wheelsMove(1, 0, iSpeed);
+        wheelsMove(1, 0, mySpeed);
         rotate = random(400, 800);
         delay(rotate);
       }
@@ -159,7 +119,7 @@ void loop() {
 
     //движение назад
     if (moving == 3 || timer2 > 2) {
-      wheelsMove(0, 0, iSpeed);
+      wheelsMove(0, 0, mySpeed);
       delay(500);
       timer3 = timer3 + 1;
       timer1 = 0;
